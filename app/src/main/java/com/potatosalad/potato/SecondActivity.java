315dbe1523bb16d150b3fbf30b1a0c855eae1737
wingkitcwk <wingkitcwk@gmail.com>
2016-10-05 22:31:00 +0800

    first commit

--- app/src/main/java/com/potatosalad/potato/SecondActivity.java
+++ app/src/main/java/com/potatosalad/potato/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.potatosalad.potato;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -11,6 +12,9 @@ public class SecondActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        intent.getBooleanArrayExtra()
 
         setContentView(R.layout.second_main);
     }
